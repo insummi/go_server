b488583506f0549bcbb1c639522f8dcc780dad2c
Mikhail Susakin <susakin@gmail.com>
2024-01-20 22:03:48 +0300

    os Getenv

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 
@@ -10,12 +11,22 @@ import (
 )
 
 func main() {
+	var port = envPortOr("3000")
 	h := handler.NewHandler()
 	r := chi.NewRouter()
 
 	r.Get("/", h.Hello)
 	log.Print("Starting Server")
-	err := http.ListenAndServe(":3000", r)
+	err := http.ListenAndServe(port, r)
 	log.Fatal(err)
 	log.Print("Shutting server down")
+}
+
+func envPortOr(port string) string {
+	// If `PORT` variable in environment exists, return it
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		return ":" + envPort
+	}
+	// Otherwise, return the value of `port` variable from function argument
+	return ":" + port
 }
